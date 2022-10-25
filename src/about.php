11b8868e6409e7a1d484dfb6a5d775cef54b4ea0
Nate <natemans01@gmail.com>
2022-10-25 18:49:29 -0400

    Begin converting html files to php

---
+++ src/about.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <title>Title</title>
+        <link rel="stylesheet" href="./css/style.css">
+    </head>
+    <body>
+        <?PHP
+            require('header.php');
+        ?>
+        <div id="main">
+            <h2>Sample Title Text</h2>
+            <p>Text</p>
+        </div>
+    </body>
+</html>
---
+++ src/header.php
@@ -0,0 +1,15 @@
+<header>
+    <div id="title">
+        <h1>( ✓ ) Podium Check</h1>
+        <div id="user">
+            Welcome back, user.
+            <img src="./img/offwhite-user-png.png">
+        </div>
+    </div>
+    <div id="nav">
+        <a href="index.php">Home</a>
+        <a href="create.html">Podium Check Form</a>
+        <a href="explore.html">Analytics</a>
+        <a href="about.html">About</a>
+    </div>
+</header>
