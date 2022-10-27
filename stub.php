12d8ff85906020a49683ae99755d2916ad13001d
Nate <natemans01@gmail.com>
2022-10-27 08:41:01 -0400

    Switched files over to php

---
+++ stub.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <title>Title</title>
+        <link rel="stylesheet" href="./css/style.css">
+    </head>
+    <body>
+        <?PHP
+            require('header.php');
+        ?>
+        <div id="main">
+            <h2>You've reached a stub!</h2>
+            <p>We're still working on this page, but check back soon!</p>
+        </div>
+    </body>
+</html>
