ff320313c9b6a929a222a0ad8e36d1c1ea30cdd7
Nate <natemans01@gmail.com>
2022-10-27 21:19:19 -0400

    Added documentation to about page

--- about.php
+++ about.php
@@ -13,7 +13,11 @@
             <p>This site was created by Noah George, Nate Mansfield, Nick Nolt, and Ethan Weitkamp as our semester project for CS341.</p>
             <p>It serves to improve upon the ITS department's process for completing podium checks, which ensure that classroom audio-video technology is functioning properly.</p>
             <h2>Documentation</h2>
-            <p><a href=""></a></p>
+            <ul>
+                <li><a href="./docs/user-stories.pdf">User Stories</a></li>
+                <li><a href="https://tasks.office.com/elizabethtown.onmicrosoft.com/Home/PlanViews/GsCtgWu2S0q23YETr7VrKGQABEsM?Type=PlanLink&Channel=Link&CreatedTime=638025151759570000">Task Board</a></li>
+                <li><a href="./docs/ERD.png">ERD Model</a></li>
+            </ul>
         </div>
     </body>
 </html>
