3d1d8e0e147be53569305a8a1ad37f20cea4403c
Nick <noltn@etown.edu>
2022-11-01 10:36:13 -0400

    form test

--- form.php
+++ form.php
@@ -1,1 +1,14 @@
 <?php header("Location: stub.php"); ?>
+
+<form>
+  <label for="proj1norm">Projector 1 normal hours:</label><br>
+  <input type="text" id="fname" name="fname"><br>
+  <label for="proj2norm">Projector 2 normal hours:</label><br>
+  <input type="text" id="lname" name="lname">
+
+
+
+
+
+  <input type="submit" value="Submit">
+</form> 
