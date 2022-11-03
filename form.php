c0ec4a052b17f0df3e492dd14544a2b68d44b643
Nick <noltn@etown.edu>
2022-11-03 08:20:20 -0400

    form update

--- form.php
+++ form.php
@@ -1,5 +1,7 @@
 <?php header("Location: stub.php"); ?>
 
+//If the name attribute is omitted, the value of the input field will not be sent
+
 <form>
   <label for="proj1norm">Projector 1 normal hours:</label><br>
   <input type="text" id="fname" name="fname"><br>
@@ -7,6 +9,12 @@
   <input type="text" id="lname" name="lname">
 
 
+  <input type="checkbox" id="check1" value="">
+  <label for="check1"> Laptop Ethernet is working</label><br>
+  <input type="checkbox" id="check2" value="Car">
+  <label for="check2"> Laptop HDMI is working</label><br>
+  <input type="checkbox" id="check3" value="Boat">
+  <label for="check3"> Laptop USB-C is working</label>
 
 
 
