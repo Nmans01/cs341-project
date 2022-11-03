9dbc04ff05198c9f8e711d30d84e264cd0998a85
Nate <natemans01@gmail.com>
2022-11-03 08:34:58 -0400

    minor text

--- index.php
+++ index.php
@@ -1,1 +1,25 @@
-<?php header("Location: stub.php"); ?>
+<?php //header("Location: stub.php"); ?>
+<html>
+<head>
+    <title>Stub!</title>
+    <link rel="stylesheet" href="./css/style.css">
+    <?PHP
+    require('head.php');
+    ?>
+</head>
+
+<body>
+    <?PHP
+    require('header.php');
+    ?>
+    <main id="main">
+        <h2>Changlog</h2>
+        <p>We're still working on this page, but check back soon!</p>
+        <h2>Calendar</h2>
+        <p>We're still working on this page, but check back soon!</p>
+    </main>
+    <?PHP
+    require('footer.php');
+    ?>
+</body>
+</html>
