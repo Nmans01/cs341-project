915e60dc5c018cb1843906e4d2dc203d9e8cb586
Nate <natemans01@gmail.com>
2022-11-07 14:51:35 -0500

    Fixed formatting for form page

--- form.php
+++ form.php
@@ -19,18 +19,30 @@
   <main id="main">
     <h2>Podium Check Form</h2>
     <form>
-      <fieldset><label for="room">Room
-          <input type="text" id="room" name="room"></label></fieldset>
+      <fieldset>
+        <div class='form-row'>
+          <label for="room">Room</label>
+          <input type="text" id="room" name="room">
+        </div>
+      </fieldset>
       <fieldset>
         <legend>Projector Hours</legend>
-        <label for="proj1norm">Projector 1 normal hours:
-          <input type="number" id="proj1norm" name="proj1norm"></label>
-        <label for="proj2eco">Projector 1 eco hours:
-          <input type="number" id="proj1eco" name="proj1eco"></label>
-        <label for="proj1norm">Projector 2 normal hours:
-          <input type="number" id="proj2norm" name="proj2norm"></label>
-        <label for="proj2eco">Projector 2 eco hours:
-          <input type="number" id="proj2eco" name="proj2eco"></label>
+        <div class="form-row">
+          <label for="proj1norm">Projector 1 normal hours:</label>
+          <input type="number" id="proj1norm" name="proj1norm">
+        </div>
+        <div class="form-row">
+          <label for="proj2eco">Projector 1 eco hours:</label>
+          <input type="number" id="proj1eco" name="proj1eco">
+        </div>
+        <div class="form-row">
+          <label for="proj1norm">Projector 2 normal hours:</label>
+          <input type="number" id="proj2norm" name="proj2norm">
+        </div>
+        <div class="form-row">
+          <label for="proj2eco">Projector 2 eco hours:</label>
+          <input type="number" id="proj2eco" name="proj2eco">
+        </div>
       </fieldset>
       <!-- add normal + eco hours together for a total hour calculation -->
 
@@ -38,14 +50,22 @@
       <!-- Check of Laptop inputs -->
       <fieldset>
         <legend>Laptop Inputs</legend>
-        <label for="lapcheck1"> Laptop Ethernet works
-          <input type="checkbox" id="LapCheck1" value="LapEthernet"></label>
-        <label for="lapcheck2"> Laptop HDMI works
-          <input type="checkbox" id="LapCheck2" value="LapHdmi"></label>
-        <label for="lapcheck3"> Laptop USB-C works
-          <input type="checkbox" id="LapCheck3" value="LapUsbc"></label>
-        <label for="lapcheck4"> Laptop VGA works
-          <input type="checkbox" id="LapCheck4" value="LapVga"></label>
+        <div class="form-row">
+          <label for="LapCheck1"> Laptop Ethernet works</label>
+          <input type="checkbox" id="LapCheck1" value="LapEthernet">
+        </div>
+        <div class="form-row">
+          <label for="lapcheck2"> Laptop HDMI works</label>
+          <input type="checkbox" id="LapCheck2" value="LapHdmi">
+        </div>
+        <div class="form-row">
+          <label for="lapcheck3"> Laptop USB-C works</label>
+          <input type="checkbox" id="LapCheck3" value="LapUsbc">
+        </div>
+        <div class="form-row">
+          <label for="lapcheck4"> Laptop VGA works</label>
+          <input type="checkbox" id="LapCheck4" value="LapVga">
+        </div>
       </fieldset>
 
       <input type="submit" value="Submit">
