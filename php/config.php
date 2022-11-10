bfb1ecc6e8fdeddf646b0851677d51ea4672b837
Nate <natemans01@gmail.com>
2022-11-09 23:09:27 -0500

    Fixed an oopsie

--- php/config.php
+++ php/config.php
@@ -2,8 +2,8 @@
 $databasehost = "156.67.74.51";
 $database = "u413142534_project8";
 $databaseuser = "u413142534_python";
-$databasepassword = "M0delzR@wesome";
+$databased = "M0delzR@wesome";
 $salt1 = "cobra";
 $salt2 = "Python";
 
-$db = mysqli_connect($databasehost,$databaseuser,$databasepassword,$database);
+$db = mysqli_connect($databasehost,$databaseuser,$databased,$database);
