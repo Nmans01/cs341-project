ce1b5dff57465ab580b43e8a605eb9a68f0d1ede
Nate <natemans01@gmail.com>
2022-11-30 16:44:35 -0500

    Admin page changes

--- admin.php
+++ admin.php
@@ -8,6 +8,7 @@
         <?PHP
             require('head.php');
         ?>
+        <link rel="stylesheet" href="./css/admin.css">
     </head>
     <body>
         <?PHP
@@ -15,14 +16,47 @@
         ?>
         <main id="main">
             <h2>Admin</h2>
-            <h3>Edit Users</h3>
-            <table>
-                
-            </table>
-            <h3>Edit Rooms</h3>
-            <table>
+            <div id='headerLinks'>
+                <a>Edit Room Groups</a> |
+                <a>Edit Room Attributes</a>
+            </div>
+            <section>
+                <h3>Edit Users</h3>
+                <div class='adminRow'>
+                    <input type='text' placeholder="Search for a user...">
+                    <div>
+                        <button>Add User</button>
+                        <button>Remove User</button>
+                    </div>
+                </div>
+                <table>
+                    <tr></tr>
+                </table>
+                <form action="">
 
-            </table>
+                </form>
+            </section>
+            <section>
+                <h3>Edit Rooms</h3>
+                <div class='adminRow'>
+                    <div>
+                        <select>
+                            <option value="">Select a group...</option>
+                        </select>
+                        <input type='text' placeholder="Search for a room...">
+                    </div>
+                    <div>
+                        <button>Add Room</button>
+                        <button>Remove Room</button>
+                    </div>
+                </div>
+                <table>
+                    <tr></tr>
+                </table>
+                <form action="">
+                    
+                </form>
+            </section>
         </main>
         <?PHP
             require('footer.php');
